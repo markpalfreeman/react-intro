f21d0d5b9d9d236209abbacf10e4bccd54255a0b
Mark Palfreeman <markpalfreeman@gmail.com>
2017-01-18 10:18:30 -0800

    Add search input w/ filter based on search

--- js/Search.js
+++ js/Search.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+
+  handleSearchTermChange (event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  },
+
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <div className='show-card'>
-              <img src={`/public/img/posters/${show.poster}`} />
-              <div>
-                <h3>{show.title}</h3>
-                <h4>({show.year})</h4>
-                <p>{show.description}</p>
-              </div>
-            </div>
-          )
-        })}
+        <header>
+          <h1>mvidz</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows
+            .filter(show => {
+              // is the search term found in either the title or description?
+              return `${show.title} ${show.description}`.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) >= 0
+            })
+            .map(show => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+              )
+            }
+          )}
+        </div>
       </div>
     )
   }
